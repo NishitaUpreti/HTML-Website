54666f571dccebfec52c9ee954e9700777e7d84f
Nishita Upreti <nishitaupreti@Nishitas-MacBook-Air.local>
2024-11-26 13:16:04 +0530

    firstcommit

---
+++ html website/script.js
@@ -0,0 +1,13 @@
+function startExploring() {
+    window.location.href = "#"; // Replace '#' with the URL of the page you want to link to
+}
+function startExploring() {
+    window.location.href = "#"; // Replace '#' with the URL of the page you want to link to
+}
+
+function openPost(page) {
+    window.open(page, "_blank"); // Opens the page in a new tab
+}
+function openAddBlogPage() {
+    window.location.href = 'add-blog.html'; // Redirects to the add blog page
+}
